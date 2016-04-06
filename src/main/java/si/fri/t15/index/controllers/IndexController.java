0a0873a5411c7050ef6e2de3ec8f1f706be83c58
Rok Zidarn <rz3817@student.uni-lj.si>
2016-04-06 11:25:32 +0200

    dodal root page za vse uporabnike, signup za paciente je koncan, potreben se signin

---
+++ src/main/java/si/fri/t15/index/controllers/IndexController.java
@@ -0,0 +1,21 @@
+package si.fri.t15.index.controllers;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import si.fri.t15.base.controllers.ControllerBase;
+
+@Controller
+public class IndexController extends ControllerBase{
+	
+	@RequestMapping(value = "/")
+	public String index(Model model, HttpServletRequest request) {
+		model.addAttribute("path", "/mediko_dev/");
+		model.addAttribute("title", "MEDIKO");
+		return "index";
+	}
+}
