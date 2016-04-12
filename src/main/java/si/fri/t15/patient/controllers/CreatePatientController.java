0bfba22b14def2d3ad6eb93e4db8badde29913e0
Žan Pevec <pevec@outlook.com>
2016-04-13 00:52:17 +0200

    Dodana kontrola in template

---
+++ src/main/java/si/fri/t15/patient/controllers/CreatePatientController.java
@@ -0,0 +1,30 @@
+package si.fri.t15.patient.controllers;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import si.fri.t15.base.controllers.ControllerBase;
+
+@Controller
+public class CreatePatientController extends ControllerBase{
+	
+	@RequestMapping(value = "patient/createPatient")
+	public String home(Model model, HttpServletRequest request) {
+		//model.addAttribute("login", "login");
+		//model.addAttribute("_csrf", (CsrfToken) request.getAttribute(CsrfToken.class.getName()));
+		//model.addAttribute("trans", getTranslation("login.title", request));
+		
+		//Side menu variables
+		model.addAttribute("usertype", "user");
+		model.addAttribute("page", "patient");
+		model.addAttribute("subpage", "createPatient");	
+		model.addAttribute("path", "/mediko_dev/");
+		//Page variables
+		model.addAttribute("title", "Dodaj pacient");
+		return "createPatient";
+	}
+}
