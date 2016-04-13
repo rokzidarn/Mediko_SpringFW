8d77ed790ec825abbc534bc9e4fb453ee7c9d6c2
Žan Pevec <pevec@outlook.com>
2016-04-13 09:49:16 +0200

    dodan _csrf
    Vstavljanje še ne deluje

--- src/main/java/si/fri/t15/patient/controllers/CreatePatientController.java
+++ src/main/java/si/fri/t15/patient/controllers/CreatePatientController.java
@@ -1,19 +1,25 @@
 package si.fri.t15.patient.controllers;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import si.fri.t15.base.controllers.ControllerBase;
+import si.fri.t15.validators.SignUpForm;
 
 @Controller
 public class CreatePatientController extends ControllerBase{
 	
-	@RequestMapping(value = "patient/createPatient")
-	public String home(Model model, HttpServletRequest request) {
+	@RequestMapping(value = "patient/createPatient", method=RequestMethod.GET)
+	public String createPatient(Model model, HttpServletRequest request) {
 		//model.addAttribute("login", "login");
 		//model.addAttribute("_csrf", (CsrfToken) request.getAttribute(CsrfToken.class.getName()));
 		//model.addAttribute("trans", getTranslation("login.title", request));
@@ -27,4 +33,5 @@ public class CreatePatientController extends ControllerBase{
 		model.addAttribute("title", "Dodaj pacienta");
 		return "createPatient";
 	}
+
 }
