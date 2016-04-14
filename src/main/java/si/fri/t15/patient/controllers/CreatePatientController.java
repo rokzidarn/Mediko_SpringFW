f7e5223b6d6b6f991f1d091bf075d5b07c46cac3
Žan Pevec <pevec@outlook.com>
2016-04-14 21:39:08 +0200

    Dodana validacija

--- src/main/java/si/fri/t15/patient/controllers/CreatePatientController.java
+++ src/main/java/si/fri/t15/patient/controllers/CreatePatientController.java
@@ -2,6 +2,7 @@ package si.fri.t15.patient.controllers;
 
 import java.util.List;
 
+import javax.annotation.Resource;
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
 import javax.servlet.http.HttpServletRequest;
@@ -12,6 +13,9 @@ import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.Validator;
+import org.springframework.web.bind.ServletRequestDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -19,6 +23,8 @@ import org.springframework.web.servlet.ModelAndView;
 
 import si.fri.t15.base.controllers.ControllerBase;
 import si.fri.t15.models.PO_Box;
+import si.fri.t15.validators.CreatePatientForm;
+import si.fri.t15.validators.CreatePatientValidator;
 import si.fri.t15.validators.SignUpForm;
 
 @Controller
@@ -27,8 +33,18 @@ public class CreatePatientController extends ControllerBase{
 	@Autowired
 	EntityManager em;
 	
+	@Autowired
+	CreatePatientValidator createPatientValidator;
+	
+	@InitBinder
+	protected void initBinder(HttpServletRequest request,
+			ServletRequestDataBinder binder) {
+		binder.setValidator(createPatientValidator);
+	}
+ 
+	
 	@RequestMapping(value = "/patient/createPatient", method=RequestMethod.GET)
-	public String createPatient(Model model, HttpServletRequest request) {
+	public ModelAndView createPatient(Model model, HttpServletRequest request) {
 		//model.addAttribute("login", "login");
 		//model.addAttribute("_csrf", (CsrfToken) request.getAttribute(CsrfToken.class.getName()));
 		//model.addAttribute("trans", getTranslation("login.title", request));
@@ -46,7 +62,20 @@ public class CreatePatientController extends ControllerBase{
 		Query allPOBoxQuery = em.createNamedQuery("PO_Box.findAll");
 		List<PO_Box> poBoxes = allPOBoxQuery.getResultList();
 		model.addAttribute("po_boxes",poBoxes);
-		return "createPatient";
+		
+		return new ModelAndView( "createPatient");
+	}
+	
+	@RequestMapping(value = "/patient/createPatient", method=RequestMethod.POST)
+	public ModelAndView createPatientPOST(Model model, @ModelAttribute("command") @Valid CreatePatientForm command,
+			BindingResult result, HttpServletRequest request) {
+		
+		if (result.hasErrors()) {
+			return new ModelAndView("createPatient");
+		}
+		
+		return new ModelAndView("createPatient");
+	
 	}
 
 }
---
+++ src/main/java/si/fri/t15/validators/CreatePatientValidator.java
@@ -0,0 +1,72 @@
+package si.fri.t15.validators;
+
+import org.springframework.validation.Validator;
+
+import java.util.StringTokenizer;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+
+public class CreatePatientValidator implements Validator{
+
+	private static final int MINIMUM_PASSWORD_LENGTH = 6;
+
+	@Override
+	public boolean supports(Class<?> c) {
+		return c.isAssignableFrom(CreatePatientForm.class);
+	}
+
+	@Override
+	public void validate(Object target, Errors errors) {
+		ValidationUtils.rejectIfEmpty(errors, "firstName", "field.required", "Required field");
+		ValidationUtils.rejectIfEmpty(errors, "lastName", "field.required", "Required field");
+		
+		CreatePatientForm u = (CreatePatientForm) target;
+		System.out.println(u.getFirstName());
+		/*
+		//Problem maker
+		if (u.getRepeatpassword() != null && u.getPassword() != null && !u.getRepeatpassword().equals(u.getPassword())) {
+			errors.rejectValue("passwordConfirmation", "field.format",
+					"Passwords do not match");
+		}
+		
+		if (!validateEmail(u.getUsername())) {
+			errors.rejectValue("username", "field.format",
+					"Incorrect email format");
+		}
+
+		if (u.getPassword() != null
+				&& u.getPassword().trim().length() < MINIMUM_PASSWORD_LENGTH) {
+			errors.rejectValue("password", "field.min.length",
+					new Object[] { Integer.valueOf(MINIMUM_PASSWORD_LENGTH) },
+					"The password must be at least [" + MINIMUM_PASSWORD_LENGTH
+							+ "] characters in length.");
+		}*/
+	}
+	
+	private boolean validateEmail(String email) {
+		 
+		if (email == null) {
+			return false; // email is empty
+		}
+ 
+		Pattern p = Pattern.compile(".+@.+\\.[a-z]+");
+ 
+		// Match the given string with the pattern
+		Matcher m = p.matcher(email);
+ 
+		// check whether match is found
+		boolean matchFound = m.matches();
+ 
+		StringTokenizer st = new StringTokenizer(email, ".");
+		String lastToken = null;
+		while (st.hasMoreTokens()) {
+			lastToken = st.nextToken();
+		}
+ 
+		return matchFound && lastToken.length() >= 2
+				&& email.length() - 1 != lastToken.length();
+	}
+}
