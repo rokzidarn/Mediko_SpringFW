5f6dd3d38fb6980753b285dfc1a8d1b6ba5c647e
Žan Pevec <pevec@outlook.com>
2016-04-21 21:08:16 +0200

    Dodani repositoriji za podatke o pacientu

---
+++ src/main/java/si/fri/t15/dao/DietRepository.java
@@ -0,0 +1,21 @@
+package si.fri.t15.dao;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import si.fri.t15.models.Diet;
+import si.fri.t15.models.Medicine;
+import si.fri.t15.models.user.PatientData;
+
+@Repository
+public interface DietRepository extends CrudRepository<Diet, Integer> {
+
+	@Query("SELECT c.diets FROM Checkup c "
+			+ "WHERE c.patient = :patient")
+	List<List<Medicine>> findByPatient(@Param(value = "patient") PatientData patient);
+	
+}
