cdd9cf39055bc642c259623e99d574cd7f69d321
Aleš Sladič <ales@sladic.si>
2016-05-13 14:44:01 +0200

    quick fix

--- src/main/java/si/fri/t15/checkup/controllers/CheckupController.java
+++ src/main/java/si/fri/t15/checkup/controllers/CheckupController.java
@@ -1,19 +1,27 @@
 package si.fri.t15.checkup.controllers;
 
-import javax.servlet.http.HttpServletRequest;
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
 import javax.persistence.EntityManager;
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 import si.fri.t15.base.controllers.ControllerBase;
+import si.fri.t15.models.Appointment;
 import si.fri.t15.models.Checkup;
 import si.fri.t15.models.user.DoctorData;
 import si.fri.t15.models.user.PatientData;
 import si.fri.t15.models.user.User;
-import org.springframework.transaction.annotation.Transactional;
 
 @Controller
 public class CheckupController extends ControllerBase {
@@ -55,6 +63,16 @@ public class CheckupController extends ControllerBase {
 		model.addAttribute("diseases", curr.getDiseases()); 
 		model.addAttribute("diets", curr.getDiets()); 
 		model.addAttribute("medicines", curr.getMedicines());
+		
+		Date now = new Date(Calendar.getInstance().getTime().getTime());
+		List<Appointment> upcoming = new ArrayList<Appointment>();
+		for (Appointment a : p.getAppointments()) {
+			Date date = a.getDate();
+			if (date.compareTo(now) > 0) {
+				upcoming.add(a);
+			}
+		}
+		model.addAttribute("appointments", upcoming);
 		
 		//Side menu variables
 		model.addAttribute("usertype", userType);
